8ff98f08c67dcf891045bc2dffee7fb024ddf2bc
Niklas <nicke.l@telia.com>
2020-09-11 15:37:04 +0200

    Implemented server hello-world

---
+++ src/server/d7024e/nodelookup.go
@@ -0,0 +1,11 @@
+package d7024e
+
+import (
+	"net"
+)
+
+// Joins a network.
+func join_network(ip string, routing_table *RoutingTable) {
+	var nodes Contact = node_lookup(routing_table.get_me().
+}
+
